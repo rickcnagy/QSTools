e1e71e6d482fb821891e0cea990ac21821fd70ea
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-05 14:58:18 -0400

    Removing...

--- api/check_section_and_enrollment_match.py
+++ api/check_section_and_enrollment_match.py
@@ -1,5 +1,5 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
-"""Check that SOURCE_SEMESTER matches the current semester in both enrollment and sections."""
+"""Putting back. Check that SOURCE_SEMESTER matches the current semester in both enrollment and sections."""
 
 
 import qs
