1d9ada573d303751b5e10a8ca0f9aef12d859362
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-07 10:27:50 -0400

    more extensible, link in entry titles to file/folder

--- utility/readme_generator.py
+++ utility/readme_generator.py
@@ -1,9 +1,11 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
-"""Generate README from docstrings in files in folder passed in via stdin
+"""Generate README from in files in folder passed in via stdin
 
 Command Line Args (in order):
     path to README directory
-    (optional) title of Markdown document
+    (optional) README title
+Example Usage:
+./utility/readme_generator.py ./api 'API Scripts'
 """
 
 import os
@@ -28,51 +30,20 @@ def generate_markdown(folder_path, title):
     dirs = walk[1]
     filenames = walk[2]
     filenames = list(set(filenames))
-    py_files = [i for i in filenames if i[-2:] == 'py']
-    js_files = [i for i in filenames if i[-2:] == 'js']
 
-    # {filename: docstring}
-    doc = {}
-
-    def set_docstring(filename, docstring):
-        docstring = docstring or ''
-        doc[filename] = docstring
-
-    for dirname in dirs:
-        doc[dirname] = 'folder'
-    for filename in py_files:
-        set_docstring(filename, parse_py(filename))
-    for filename in js_files:
-        set_docstring(filename, parse_js(filename, folder_path))
-
-    if not doc: return ''
+    entries = [ScriptEntry(folder_path, i) for i in filenames]
+    entries += [FolderEntry(folder_path, i) for i in dirs]
+    entries = [i for i in entries if i.is_valid()]
+    entries = sorted(entries, key=lambda x: x.key())
+    if not entries: return ''
 
     markdown = '{}\n===\n\n'.format(title) if title else ''
-    for filename in sorted(doc.keys()):
-        docstring = doc[filename]
-        if docstring == 'folder':
-            filename = filename + '/'
-            docstring = ''
-        markdown += '####`{}`\n\n{}\n\n'.format(filename, docstring)
+    for entry in entries:
+        markdown += '####{}\n\n'.format(entry.md_title())
+        docstring = entry.docstring()
+        markdown += '{}\n\n'.format(docstring) if docstring else ''
     markdown = markdown.strip()
     return markdown
-
-
-def parse_py(filename):
-    mod_name = filename.replace('.py', '')
-    return __import__(mod_name).__doc__
-
-
-def parse_js(filename, folder_path):
-    """Matches javadocs like: /** this is a javadoc! */"""
-    with open(folder_path + '/' + filename) as f:
-        match = re.findall(r'/\*\*\s+(.+)\s+\*\/', f.read(), flags=re.DOTALL)
-        if match:
-            javadoc = match[0]
-            javadoc = javadoc.replace('*', '')
-            javadoc = javadoc.replace('#', '\#')
-            javadoc = re.sub('\s+', ' ', javadoc)
-            return javadoc
 
 
 def test():
@@ -82,5 +53,70 @@ def test():
     )
 
 
+class ReadmeEntry(object):
+
+    def __init__(self, folder_path, name):
+        self.folder_path = folder_path
+        self.name = name
+
+    def key(self):
+        return ''.join([i.lower() for i in self.name if i.isalnum()])
+
+    def md_title(self):
+        return '[`{}`]({})'.format(self.name, self.repo_link())
+
+    def repo_link(self):
+        path = self.folder_path.split('/')
+        start = path.index('QSTools')
+        dir_path = '/'.join([dir_name for i, dir_name in enumerate(path) if i > start])
+        dir_path = dir_path + '/' + self.name
+        return '../' + dir_path
+
+    def docstring(self):
+        pass
+
+    def is_valid(self):
+        return True
+
+
+class FolderEntry(ReadmeEntry):
+
+    def title(self):
+        return self.name + '/'
+
+
+class ScriptEntry(ReadmeEntry):
+
+    def is_valid(self):
+        return self.is_py() or self.is_js()
+
+    def is_py(self):
+        return self.name[-3:] == '.py'
+
+    def is_js(self):
+        return self.name[-3:] == '.js'
+
+    def docstring(self):
+        if self.is_py():
+            return self.py_docstring()
+        elif self.is_js():
+            return self.javadoc()
+
+    def py_docstring(self):
+        mod_name = filename.replace('.py', '')
+        return __import__(mod_name).__doc__
+
+    def javadoc(self):
+        """Matches javadocs like: /** this is a javadoc! */"""
+        with open(self.folder_path + '/' + self.name) as f:
+            match = re.findall(r'/\*\*\s+(.+)\s+\*\/', f.read(), flags=re.DOTALL)
+            if match:
+                javadoc = match[0]
+                javadoc = javadoc.replace('*', '')
+                javadoc = javadoc.replace('#', '\#')
+                javadoc = re.sub('\s+', ' ', javadoc)
+                return javadoc
+
+
 if __name__ == '__main__':
-    main()
+    test()
