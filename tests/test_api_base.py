74cda646553ffc3cd87e18bc30bf2d09206187f0
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-08 15:48:59 -0400

    setup basic testing before adding travis

--- tests/test_api_base.py
+++ tests/test_api_base.py
@@ -7,17 +7,23 @@ import api_logging
 
 
 class TestBasicGet(unittest.TestCase):
-    api_logging.silent = True
 
     class TestRequest(api_base.BaseRequest):
         base_url = 'https://api.github.com'
 
     def setUp(self):
-        self.request = self.TestRequest('Test Request', '/users/{}/repos'.format('br1ckb0t'))
+        api_logging.silent = True
+        self.request = self.TestRequest(
+            'Test Request',
+            '/users/{}/repos'.format('br1ckb0t'))
         self.request.make_request()
 
     def test_success(self):
         self.assertTrue(self.request.successful)
+
+    def test_content_is_correct(self):
+        by_id = {i['id']: i for i in self.request.data}
+        self.assertTrue(by_id[21495975]['name'], 'QSTools')
 
 if __name__ == '__main__':
     unittest.main()
