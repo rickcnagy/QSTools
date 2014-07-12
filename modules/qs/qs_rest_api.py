62d43feb145d2724c6c485f9036028676d765576
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-12 11:40:03 -0400

    begin implementation of qs.API()

--- modules/qs/qs_rest_api.py
+++ modules/qs/qs_rest_api.py
@@ -1,6 +1,7 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 
-from qs import *
+import qs
+import re
 
 
 class API(object):
@@ -8,10 +9,35 @@ class API(object):
     One API object should be created per server/subdomain - i.e. one object for
     school A on the live server, another for school B on the live server, and a
     third for school A on the backup server.
+
+    Attributes:
+        schoolcode: the schoolcode being accessed
+        api_key: the API key used for access
+        live: whether or not to use the live server
     """
 
-    def __init__(self, schoolcode, live=True):
-        pass
+    def __init__(self, access_key, live=True):
+        """access_key can either be a schoolcode or API key"""
+        self._access_key = access_key
 
     def get_students(self):
         pass
+
+    def _process_access_key(self, access_key):
+        """Processes an access key, which could be a schoolcode or API key
+        Note that API keys are like schoolcode.blahblahblah
+
+        Returns:
+            A tuple, with the schoolcode followed by the API key
+        """
+        # api keys are like schoolcode.blahblahblah
+        match = re.match(r'(.+)\.', access_key)
+        if match:
+            return re.groups()[0], access_key
+        else:
+            return access_key, self._get_api_key(access_key)
+
+    def _get_api_key(self, schoolcode):
+        """Get the API key on file for that schoolcode"""
+        #TODO: implement
+
