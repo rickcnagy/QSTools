43558d4a1ab440282ef7ccd0ab8c9588935b6d63
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-14 09:35:16 -0400

    improve tests for qs.util

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,5 +4,8 @@ import qs
 import json
 
 def test_dumps():
-    obj = [3, {1: [4, 5], 2: 2}, '5']
-    assert json.dumps(obj, indent=4) == qs.util.dumps(obj)
+    complex_obj = [3, {1: [4, 5], 2: 2}, '5']
+    assert json.dumps(complex_obj, indent=4) == qs.dumps(complex_obj)
+    simple_obj = [{2: 1}, 5]
+    simple_obj_json = '[\n    {\n        "2": 1\n    }, \n    5\n]'
+    assert simple_obj_json == qs.dumps(simple_obj)
