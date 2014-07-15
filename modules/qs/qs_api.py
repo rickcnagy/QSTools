46dc6328070e5789dc3c0aa0eb17ba1f85f97626
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-14 21:08:36 -0400

    implement qs.make_request()

--- modules/qs/qs_api.py
+++ modules/qs/qs_api.py
@@ -2,6 +2,7 @@
 
 import qs
 import re
+from qs import QSRequest
 
 
 class QSAPIWrapper(qs.APIWrapper):
@@ -15,12 +16,21 @@ class QSAPIWrapper(qs.APIWrapper):
     Args:
         access_key: An API key or schoolcode to access the school by. If a
             schoolcode is supplied, its API key will be retrivied via
-            qs.api_keys. If an API key is supplied, it will both be used here
-            and then stored in the API key store for future use.
+            qs.api_keys.
+            If an API key is supplied, it will both be used here
+            and then stored in the API key store for future use *if the request
+            is successful*.
+            The access_key defaults to `qstools` for convenience while testing,
+            but since it's the first argument, `qs.API('someschool')` works to
+            set a different school.
         live: Whether or not to use the live server.
+
+    Note: in all instance methods, pass critical=True as a keyword argument to
+    make any requests generated in that method critical, so that the script
+    will exit (via logger.critical) if they fail.
     """
 
-    def __init__(self, access_key, live=True):
+    def __init__(self, access_key='qstools', live=True):
         self._access_key = access_key
         self.live = live
 
@@ -28,6 +38,33 @@ class QSAPIWrapper(qs.APIWrapper):
         self.api_key = None
 
         self._parse_access_key()
+
+    # ============
+    # = Students =
+    # ============
+
+    def get_students(self, desc='GET all students', **kwargs):
+        """Get a list of all enrolled students from /students."""
+        request = QSRequest(desc, '/students')
+        return self.make_request(request, kwargs)
+
+    def make_request(self, request, original_kwargs):
+        """Process any QSRequest in this class and make it.
+
+        Args:
+            request: the prepared (but not yet made) QSRequest
+            original_kwargs: the **kwargs passed to the function that
+                instantiated this request, such as self.get_students().
+        Returns:
+            the data from the completed
+        """
+        request.set_api_key(self.api_key)
+        if 'critical' in original_kwargs:
+            request.critical = original_kwargs['critical']
+        request.make_request()
+        if request.successful:
+            qs.api_keys.set(self._api_key_store_key_path(), self.api_key)
+        return request
 
     def _parse_access_key(self):
         """Parses self._access key, which could be a schoolcode or API key, and
@@ -38,7 +75,6 @@ class QSAPIWrapper(qs.APIWrapper):
         if match:
             self.schoolcode = match.groups()[0]
             self.api_key = self._access_key
-            qs.api_keys.set(self._api_key_store_key_path(), self._access_key)
         else:
             self.schoolcode = self._access_key
             self.api_key = qs.api_keys.get(self._api_key_store_key_path())
--- tests/test_qs_api.py
+++ tests/test_qs_api.py
@@ -4,6 +4,16 @@ import qs
 import config
 
 
+def test_make_request():
+    request = qs.QSRequest('Testing', '/students')
+    q = qs.API()
+    q.make_request(request, {'critical': True})
+    assert_equals(request.api_key, config.API_KEY)
+    assert_in('apiKey', request.params)
+    assert_equals(request.params['apiKey'], config.API_KEY)
+    assert_true(request.critical)
+
+
 def test_api_key_as_access_key():
     q = qs.API(config.API_KEY)
     assert q.schoolcode == 'qstools'
