bf673e285f17ca5ce8fb13c17e36aaba0b0c5828
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-16 12:36:45 -0400

    implement rest_cache with tests (not in use yet)

--- modules/qs/rest_cache.py
+++ modules/qs/rest_cache.py
@@ -1,37 +1,78 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
-"""Custom cache for the QS package, centered around caching REST responses."""
-
+"""Custom caching for the QS package, centered around caching REST responses
+in memory.
+"""
 
 import qs
 
 
 class RestCache(object):
-    """Class for caching REST API responses."""
+    """Interface for caching REST objects.
+
+    Note that the objects returned are often the actual _data object, so
+    generally the objects should be treated as read only to prevent corrupting
+    the cache.
+    """
 
     def __init__(self):
-        pass
+        self._data = self._default_data()
 
-    def get(self, key):
-        """Access a key in the cache."""
-        pass
+    def get(self):
+        """Retrieve the entire cache."""
+        return self._data
 
-    def set(self, key):
-        """Set a key in the cache."""
-        pass
+    def add(self, data):
+        """Add data to the cache."""
+        self._data = data
 
-    def invalidate(self, key=None):
-        """Invalidate the cache.
-        If key is supplied, only that key will be invalidated.
-        """
-        if key:
-            _invalidate_key(key)
+    def invalidate(self):
+        """Invalidate the cache."""
+        self._data = self._default_data()
+
+    def _default_data(self):
+        """The default value for _data after invalidation or __init__"""
+        return None
+
+
+class ListWithIDCache(RestCache):
+    """A RestCache where self._data is a dict with the keys matching an id key
+    in the contained dicts, but sends and receives data in flat lists.
+
+    Args:
+        id_key: The unique key that all entries in the dict will have - as in
+            the id in 'list with id'.
+        sort_key: An optional key to sort entries by when getting them.
+    """
+
+    def __init__(self, id_key='id', sort_key=None):
+        super(ListWithIDCache, self).__init__()
+        self._sort_key = sort_key
+        self._id_key = id_key
+
+    def get(self, identifier=None, by_id=False):
+        """Return a flattened list of the data or a single entry by id if id is
+        specified."""
+        if not self._data: return self._data
+        elif by_id: return self._data
+        elif identifier: return self._data.get(qs.clean_id(identifier))
         else:
-            _invalidate_cache()
+            return_data = [v for k, v in self._data.iteritems()]
+            if self._sort_key:
+                return sorted(return_data, key=lambda x: x[self._sort_key])
+            return return_data
 
-    def _invalidate_key(self, key):
-        """Invalidate a specific key."""
-        pass
+    def add(self, new_data):
+        """Add to the cache with a list or single dict. Like list.append."""
+        if type(new_data) not in [dict, list]:
+            raise TypeError('new_data must be a dict or list, not {}'.format(
+                type(new_data)))
+        elif (type(new_data) is list
+                and not all(type(i) is dict for i in new_data)):
+            raise TypeError('new_data must contain only dicts')
+        elif type(new_data) is dict:
+            new_data = [new_data]
 
-    def _invalidate_cache(self):
-        """Invalidate the entire cache."""
-        pass
+        self._data.update({qs.clean_id(i[self._id_key]): i for i in new_data})
+
+    def _default_data(self):
+        return {}
---
+++ tests/test_rest_cache.py
@@ -0,0 +1,54 @@
+"""Test the rest_cache module."""
+
+import qs
+from nose.tools import *
+
+def test_rest_cache_class():
+    cache = qs.RestCache()
+    some_string = qs.rand_str()
+
+    cache.add(some_string)
+    assert_equals(cache.get(), some_string)
+
+    cache.invalidate()
+    assert_is_none(cache.get())
+
+
+def test_list_with_id_cache_basics():
+    cache = qs.ListWithIDCache()
+
+    cache.add({'id': 12345})
+    assert_equals(cache.get(), [{'id': 12345}])
+    assert_equals(cache.get(12345), {'id': 12345})
+    assert_equals(cache.get(by_id=True), {'12345': {'id': 12345}})
+
+    cache.invalidate()
+    assert_equals(cache.get(), {})
+
+
+def test_list_with_id_cache_sorting():
+    cache = qs.ListWithIDCache(sort_key='sort')
+    unsorted = [{
+        'id': 12345,
+        'sort': 02,
+    }, {
+        'id': 146,
+        'sort': 01
+    }]
+    sorted_version = [{
+        'id': 146,
+        'sort': 01
+    },{
+        'id': 12345,
+        'sort': 02,
+    }]
+
+    cache.add(unsorted)
+    assert_equals(cache.get(), sorted_version)
+
+def test_list_with_id_cache_validation():
+    cache = qs.ListWithIDCache()
+    with assert_raises(TypeError):
+        cache.add("this will error, since it's a string")
+    with assert_raises(TypeError):
+        cache.add(["this too, since it's a string, not a dict, in a list"])
