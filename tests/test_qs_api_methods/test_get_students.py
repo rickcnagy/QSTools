612a9dd86330bec31235ce24d2ddad2771694099
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-16 15:53:07 -0400

    refactor tests for get_students to build

--- tests/test_qs_api_methods/test_get_students.py
+++ tests/test_qs_api_methods/test_get_students.py
@@ -15,21 +15,21 @@ def test_get_students():
     students = q.get_students()
 
     assert_is_instance(students, list)
+    assert_greater(len(students), 0)
     assert_equals(len(students), qs.mock_data.STUDENT_COUNT)
 
     api_mock_student = q.get_students(by_id=True)[mock_student['id']]
     assert_equals(api_mock_student['fullName'], mock_student['fullName'])
 
 
+def test_get_students_by_id():
+    by_id = q.get_students(by_id=True)
+    assert_is_instance(by_id, dict)
+    assert_greater(len(by_id), 0)
+    assert_is_instance(by_id[by_id.keys()[0]], dict)
+
+
 def test_get_student():
     student = q.get_student(mock_student['id'])
     assert_equals(student['fullName'], mock_student['fullName'])
-    assert_greater(len(q.get_students(by_id=True)), 0)
     assert_equals(student, q.get_students(by_id=True)[mock_student['id']])
-
-
-def test_get_students_by_id():
-    by_id_from_api = q.get_students(by_id=True)
-    assert_is_instance(by_id_from_api, dict)
-    by_id = {i['id']: i for i in q.get_students()}
-    assert_equals(by_id_from_api, by_id)
