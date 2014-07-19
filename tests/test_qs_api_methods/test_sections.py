274208fd3d6eaa22c1b742f509c37c9edad35bc6
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-19 11:20:17 -0400

    finish tests for get_sections

--- tests/test_qs_api_methods/test_sections.py
+++ tests/test_qs_api_methods/test_sections.py
@@ -25,14 +25,28 @@ def test_get_section_by_id():
     assert_equals(by_id[SECTION_ID]['sectionName'], SECTION_NAME)
 
 
-# def test_get_sections_from_other_semester():
-#     other = q.get_sections(semester_id=NON_ACTIVE_SEMESTER_1)
-#     assert_is_instance(other, list)
-#     assert_equals(len(other), NON_ACTIVE_SEMESTER_1_SECTION_COUNT)
-#
-#
-# def test_get_sections_from_other_by_id():
-#     other = q.get_sections(semester_id=NON_ACTIVE_SEMESTER_1, by_id=True)
-#     assert_is_instance(other, dict)
-#     other_name = other[NON_ACTIVE_SEMESTER_1_SECTION_ID]['sectionName']
-#     assert_equals(other_name, SECTION_NAME)
+def test_get_sections_from_other_semester():
+    other = q.get_sections(semester_id=NAS1_SEMESTER_ID)
+    assert_is_instance(other, list)
+    assert_equals(len(other), NAS1_SECTION_COUNT)
+
+
+def test_get_sections_from_other_by_id():
+    other = q.get_sections(semester_id=NAS1_SEMESTER_ID, by_id=True)
+    assert_is_instance(other, dict)
+    assert_not_equal(other, q.get_sections(by_id=True))
+    qs.pr( other)
+    other_name = other[NAS1_SECTION_ID]['sectionName']
+    assert_equals(other_name, NAS1_SECTION_NAME)
+
+
+def test_active_only():
+    q.get_sections(semester_id=NAS1_SEMESTER_ID)
+    assert_not_in(NAS1_SECTION_ID, q.get_sections(by_id=True))
+    assert_in(NAS1_SECTION_ID, q.get_sections(by_id=True, active_only=False))
+
+def test_get_all():
+    all_sections = q.get_sections(by_id=True, all_semesters=True)
+    assert_in(NAS2_SECTION_ID, all_sections)
+    assert_in(NAS1_SECTION_ID, all_sections)
+    assert_in(SECTION_ID, all_sections)
