344d5924adce410ca6bf582823ef6fef8da5d44d
Rick Nagy <rick.c.nagy@gmail.com>
2014-07-30 14:26:46 -0400

    improve separate_first_last after use

--- csv/README.md
+++ csv/README.md
@@ -5,6 +5,9 @@ CSV Scripts
 
 Extract the first and last names from a "Last, First" Full Name column.
 
+Usage:
+    ./separate_first_last {filename.csv}
+
 Requires:
     A column entitled "Full Name" where all the names are in "Last, First"
 
--- csv/separate_first_last.py
+++ csv/separate_first_last.py
@@ -1,6 +1,9 @@
 #!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
 """Extract the first and last names from a "Last, First" Full Name column.
 
+Usage:
+    ./separate_first_last {filename.csv}
+
 Requires:
     A column entitled "Full Name" where all the names are in "Last, First"
 
@@ -8,18 +11,27 @@ Outputs:
     The same CSV, but with "First Name" and "Last Name" columns added.
 """
 
-
+import sys
 import qs
 
-FILENAME = '/Users/Rick/Desktop/Student Information - Sheet.csv'
 DELIMETER = ','
 
-csv = qs.CSV(FILENAME)
 
-for row in csv:
-    full_name = row['Full Name']
-    delim_loc = full_name.index(DELIMETER)
+def main():
+    filename = sys.argv[1]
+    csv = qs.CSV(filename)
 
-    row['First Name'] = full_name[delim_loc + 1:].strip().title()
-    row['Last Name'] = full_name[:delim_loc].strip().title()
-csv.save()
+    csv.cols.insert(csv.cols.index('Full Name') + 1, 'First Name')
+    csv.cols.insert(csv.cols.index('Full Name') + 2, 'Last Name')
+    for row in csv:
+        full_name = row['Full Name']
+        delim_loc = full_name.index(DELIMETER)
+
+        row['First Name'] = full_name[delim_loc + 1:].strip().title()
+        row['Last Name'] = full_name[:delim_loc].strip().title()
+    csv.save()
+
+
+
+if __name__ == '__main__':
+    main()
