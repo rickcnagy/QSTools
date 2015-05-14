57bfaa504ae93c358c56a659e080dbbccf2824b7
Anna Nagy <annacnagy@gmail.com>
2015-05-14 16:26:20 -0400

    made add sections script for csv manipulation. also added support for catching SSL errors QS Logger.

---
+++ csv/add_section_ids.py
@@ -0,0 +1,58 @@
+""" 
+Add Subject IDs
+
+Designed to run after add_student_ids.py, which checks for
+duplicate names and gets the students' ids. This script is 
+the next step for a gradebook import and gets the ids for 
+the sections students are enrolled in. See /samples for an 
+example csv.
+
+Usage: 
+./add_student_id {schoolcode} {filename.csv}
+
+Requires:
+CSV with "Student ID" column, with an exact match to the provided
+school database for each student id.
+
+The semester for the subjects you're getting ids for is currently
+active in the school's account. 
+
+Outputs: 
+The same CSV with an "Section ID" column
+"""
+
+import sys
+import qs
+
+
+def main():
+	qs.logger.config(__file__) # log this critter
+
+
+	schoolcode = sys.argv[1]
+	filename = sys.argv[2]
+	csv_sections = qs.CSV(filename)
+	q = qs.API(schoolcode)
+
+	for csv_section_info in csv_sections:
+		section_name = csv_section_info[u'Section Name']
+		student = csv_section_info[u'Student ID']
+		print ""
+		print section_name
+		print student
+		print ""
+		section = q.match_section(
+			identifier=section_name, 
+			student_id = student,
+			match_name=True)
+		section_id = section[u'id']
+		csv_section_info['Section ID'] = section_id
+		print csv_section_info['Section ID']
+		print "*******************************"
+		print ""
+	
+	filepath = qs.unique_path(csv_sections.filepath, suffix="with section IDs")
+	csv_sections.save(filepath)
+
+if __name__ == '__main__':
+    main()
