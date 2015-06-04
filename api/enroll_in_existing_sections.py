8b2d5a55d4ec40bff405b3f3d717afa41577a102
Anna Nagy <annacnagy@gmail.com>
2015-06-04 12:55:55 -0400

    completed enrolled_in_existing_sections

--- api/enroll_in_existing_sections.py
+++ api/enroll_in_existing_sections.py
@@ -5,7 +5,8 @@ Imports enrollment from CSV for already created subjects (such as ones imported
 through the "Subject (New Style)" importer). Matches by secion code
 (abbreviation).
 
-Requires: CSV of enrollment data, semester id
+Requires: CSV of enrollment data with subject codes, semester id.
+CSV must have the following columns: Student ID, Section Code
 
 Usage: ./enroll_in_existing_sections.py {school code} {semester} filename.csv
 
@@ -25,20 +26,41 @@ def main():
     q = qs.API(schoolcode)
     csv_enrollments = qs.CSV(filename)
     section_enrollments = {}
+    student_enrollments = {}
 
     # Get Section Info
 
-    qs.logger.info("Getting section ids...",cc_print=True)
+    qs.logger.info("GETting section ids...",cc_print=True)
     sections = q.get_sections(semester_id=semester)
     for section in sections:
         section_code = section[u'sectionCode']
         section_id = section[u'id']
 
-        section_enrollments[section_code] = {"sectionCode:": section_code,
+        section_enrollments[section_code] = {"section_code": section_code,
                                              "section_id": section_id}
-    qs.pp(section_enrollments)
 
+    # Setup Enrollment Info
 
+    qs.logger.info("Retrieving enrollment info from csv...", cc_print=True)
+    for enrollment in csv_enrollments:
+        student_id = enrollment[u'Student ID']
+        section_code = enrollment[u'Section Code']
 
+        if section_code not in student_enrollments:
+            student_enrollments[section_code] = {"section_code": section_code,
+                                                 "students": list()}
+        student_enrollments[section_code]["students"].append(student_id)
+    print student_enrollments
+
+    qs.logger.info("Preparing for import...", cc_print=True)
+    for section in student_enrollments:
+        student_enrollments[section]["section_id"] = section_enrollments[section]["section_id"]
+
+    # Do the enrollment import
+    qs.logger.info("Importing...")
+    for section in qs.bar(student_enrollments):
+        students = student_enrollments[section]["students"]
+        section_id = student_enrollments[section]["section_id"]
+        new_enrollment = q.post_section_enrollment(section_id, students)
 if __name__ == '__main__':
     main()
