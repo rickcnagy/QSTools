bb78c627dec565e395e4d2dba1c9126aa885afc7
Anna Nagy <annacnagy@gmail.com>
2015-07-17 07:26:42 -0400

    polished up compare_csvs_for_new_name script
    edits to doc string, checks for correct headers, don't return csv if nothing there, cleaning printing of student names

--- utility/compare_csvs_for_new_names.py
+++ utility/compare_csvs_for_new_names.py
@@ -1,19 +1,18 @@
 """
 Compare CSV for New Names
 
-This script looks at 2 csv's of student data to find unique names. The unique
+This script looks at 2 csv's of name data to find unique names. The unique
 names are returned on a new spreadsheet, with all of their data, and the
-duplicates are excluded (though a list of these is printed out). Useful
-especially for historical students import. The second csv is compared against
-the first, i.e. the "base" is filename1.csv and the comparison is
-filename2.csv. The students that do not appear in filename1.csv but are in
-filename2.csv will be the ones returned. Assumes no duplicate students.
+duplicates are excluded (though a list of these is printed out). The second csv
+is compared against the first, i.e. the "base" is filename1.csv and the
+comparison is filename2.csv. The names that do not appear in filename1.csv
+but are in filename2.csv will be the ones returned. Assumes no duplicate names.
 
 Requires: 2 csv's, each with a Full Name column
 
-Usage: ./compare_csvs_for_new_students.py {filename1.csv} {filename2.csv}
+Usage: ./compare_csvs_for_new_names.py {filename1.csv} {filename2.csv}
 
-Returns: spreadsheet of students who appear in filename2.csv, but not in
+Returns: spreadsheet of names who appear in filename2.csv, but not in
 filename1.csv
 """
 
@@ -25,51 +24,54 @@ def main():
     qs.logger.config(__file__)
     filename1 = sys.argv[1]
     filename2 = sys.argv[2]
-    csv_orig_students = qs.CSV(filename1)
-    csv_new_students = qs.CSV(filename2)
+    csv_orig_names = qs.CSV(filename1)
+    csv_new_names = qs.CSV(filename2)
 
-    orig_students = []
-    new_students = []
-    unique_students = []
-    duplicate_students = []
+    orig_names = []
+    new_names = []
+    unique_names = []
+    duplicate_names = []
     new_csv_data = []
 
-    if 'Full Name' not in csv_orig_students.cols:
+    if 'Full Name' not in csv_orig_names.cols:
         raise ValueError("'Full Name' column required in original names csv")
-    if 'Full Name' not in csv_new_students.cols:
+    if 'Full Name' not in csv_new_names.cols:
         raise ValueError("'Full Name' column required in new names csv")
     
-    # Make lists of duplicate and unique students
+    # Make lists of duplicate and unique names
 
-    for student in csv_orig_students:
-        student_name = student['Full Name']
-        orig_students.append(student_name)
+    for name in csv_orig_names:
+        name_name = name['Full Name']
+        orig_names.append(name_name)
 
-    for student in csv_new_students:
-        student_name = student['Full Name']
-        new_students.append(student_name)
+    for name in csv_new_names:
+        name_name = name['Full Name']
+        new_names.append(name_name)
 
-    for name in new_students:
+    for name in new_names:
         qs.logger.info(name, cc_print=False)
-        if name in orig_students:
-            duplicate_students.append(name)
+        if name in orig_names:
+            duplicate_names.append(name)
         else:
-            unique_students.append(name)
+            unique_names.append(name)
 
-    # Write CSV of unique students
+    # Write CSV of unique names
 
-    for student in csv_new_students:
-        student_name = student['Full Name']
-        if student_name in unique_students:
-            new_csv_data.append(student)
+    for name in csv_new_names:
+        name_name = name['Full Name']
+        if name_name in unique_names:
+            new_csv_data.append(name)
 
-    qs.logger.info('Duplicate Students (not returned in output csv):',
+    qs.logger.info('Duplicate names ({}) (not returned in output csv):' .format(len(duplicate_names)),
                    cc_print=True)
-    qs.logger.info(duplicate_students, cc_print=True)
-    qs.logger.info('Unique Students (returned):', cc_print=True)
-    qs.logger.info(unique_students, cc_print=True)
+    qs.logger.info(qs.dumps(duplicate_names), cc_print=True)
+    qs.logger.info('Unique names ({}) (returned):' .format(len(unique_names)), cc_print=True)
+    qs.logger.info(qs.dumps(unique_names), cc_print=True)
 
-    qs.write_csv(new_csv_data, '~/Desktop/new_students.csv')
+    if unique_names:
+        qs.write_csv(new_csv_data, '~/Desktop/unique_names.csv')
+    else:
+        qs.logger.info("No file generated.", cc_print=True)
 
 if __name__ == ('__main__'):
     main()
